e951199cbdf4735f42a273b7b1a534de93abab48
root <root@vultr.guest>
2026-08-14 07:40:24 +0000

    app 1.0.11: notificaciones de conexión y jornada de trabajo

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.dmujeres.traccar"
         minSdk = 26
         targetSdk = 34
-        versionCode = 11
-        versionName = "1.0.10"
+        versionCode = 12
+        versionName = "1.0.11"
     }
 
     buildTypes {
