52d5b3d5c7ef4f0b0ca4e0120568fc446d6d7b66
root <root@vultr.guest>
2026-08-17 02:47:39 +0000

    app 1.0.24: icono centrado

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.dmujeres.traccar"
         minSdk = 26
         targetSdk = 35
-        versionCode = 24
-        versionName = "1.0.23"
+        versionCode = 25
+        versionName = "1.0.24"
     }
 
     buildTypes {
