d48af4375f68f36ba656847a4cc2bc4876e84299
sekaishopml <sekaishopml@users.noreply.github.com>
2026-08-31 01:31:56 -0500

    app: release v1.0.38 y metadatos de actualizacion

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.dmujeres.traccar"
         minSdk = 26
         targetSdk = 35
-        versionCode = 38
-        versionName = "1.0.37"
+        versionCode = 39
+        versionName = "1.0.38"
     }
 
     buildTypes {
