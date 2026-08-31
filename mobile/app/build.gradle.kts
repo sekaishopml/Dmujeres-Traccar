fb0fca037a56261368bee49071c97ae75e2bda32
sekaishopml <sekaishopml@users.noreply.github.com>
2026-08-31 02:43:07 -0500

    app: release v1.0.39

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.dmujeres.traccar"
         minSdk = 26
         targetSdk = 35
-        versionCode = 39
-        versionName = "1.0.38"
+        versionCode = 40
+        versionName = "1.0.39"
     }
 
     buildTypes {
